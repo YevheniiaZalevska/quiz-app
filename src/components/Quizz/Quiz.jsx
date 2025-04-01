874a2cb13fd0c3c4408738f432b126158383da6b
YevheniiaZalevska <yevhenia05@gmail.com>
2025-04-01 19:22:47 +0200

    update quiz

---
+++ src/components/Quizz/Quiz.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import styles from './Quiz.module.css';
+import { getQuestions } from '../../services/quizApi';
+
+export default function Quiz({ categoryId }) {
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const data = await getQuestions(categoryId);
+      setQuestions(data);
+      setIsLoading(false);
+    };
+    fetchQuestions();
+  }, [categoryId]);
+
+  if (isLoading) return <p>Loading questions...</p>;
+  if (!questions || questions.length === 0) return <p>No questions found.</p>;
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>First Question:</h2>
+      <p dangerouslySetInnerHTML={{ __html: questions[0].question }} />
+    </div>
+  );
+}
